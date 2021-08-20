5db59e8669df7e03865f25738ae91d4037672f93
Marcel <maas.marcel@gmail.com>
2021-08-20 16:54:27 +0200

    Moved script tags to top.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -30,11 +30,11 @@ export default function Layout({ children }) {
         <link href="/images/favicon.png" rel="shortcut icon" type="image/x-icon"></link>
         <link href="/images/webclip.png" rel="apple-touch-icon"></link>
       </Head>
+      <Script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossOrigin="anonymous"></Script>
+      <Script src="../js/webflow.js" />
       <Navbar></Navbar>
       <main>{children}</main>
       <Footer></Footer>
-      <Script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossOrigin="anonymous"></Script>
-      <Script src="../js/webflow.js" />
       <InitWebflow/>
     </>
   )
